e51c353336602dfa405f1e8fce7752f2d724bcc2
rdhtiwari <radhe.shyam@pichainlabs.com>
2022-11-06 23:13:57 +0530

    nov6

--- L09-04 Docker Compose/app.py
+++ L09-04 Docker Compose/app.py
@@ -1,6 +1,9 @@
 import time
 import redis
 from flask import Flask
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
@@ -19,6 +22,10 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
+    logger.info(f"--------info------count:{count}")
+    logger.debug(f"--------debug------count:{count}")
+    logger.critical(f"-------critical-------count:{count}")
+    logger.error(f"---------error-----count:{count}")
     return "What's up Docker Deep Divers! You've visited me {} times.\n".format(count)
 
 if __name__ == "__main__":
